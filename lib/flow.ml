4171e79a9893f41f3b4676247a7cfecae8592ef8
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-09-21 20:54:53 +0330

    1/3. Defined the DFG module: the type for the Data-flow graph was defined; 2/3. Defined the data-transfer module: the module that encompasses transfer of data in def-use and use-def chains was defined; 3/3. Renamed `Transfer`: the module `Transfer` was renamed to `ControlTransfer` for assymetry with `DataTransfer`

---
+++ lib/flow.ml
@@ -0,0 +1,34 @@
+module BasicBlock = struct
+  type t = 
+    { id: int
+    ; instrs: Instr.t list
+    }
+
+  let compare bb1 bb2 = compare bb1.id bb2.id
+  let hash bb = Hashtbl.hash bb.id
+  let equal bb1 bb2 = bb1.id = bb2.id
+end
+
+module ControlTransfer = struct
+  type t =
+    | OnZero
+    | Jump of Symtbl.label
+    | Call of Symtbl.ident
+    | Fallthrough
+
+  let compare = compare
+  let default = Fallthrough
+end
+
+module DataTransfer = struct
+  type t = 
+    | Use of Symtbl.IdentSet.t
+    | Def of Symtbl.IdentSet.t
+
+  let compare = Symtbl.IdentSet.compare
+  let default = Symtbl.IdentSet.empty
+end
+
+module CFG = Graph.Imperative.Digraph.ConcreteLabeled(BasicBlock)(ControlTransfer)
+module DFG = Graph.Imperative.Digraph.ConcreteLabeled(BasicBlock)(DataTransfer)
+
