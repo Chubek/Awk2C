ccfb848365aacd10ac2c3009aa2d7319d6418d0e
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-09-23 18:41:43 +0330

    Added, Revised, Removed: the old "Analyzes" were removed, added new attribute fields to `BasicBlock`

--- lib/flow.ml
+++ lib/flow.ml
@@ -1,17 +1,53 @@
+type blkid = int
+
 module BasicBlock = struct
   type t = 
-    { id: int
+    { id: blkid
     ; instrs: Instr.t list
+    ; mutable gen_defs: Instr.TempSet
+    ; mutable kill_defs: Instr.TempSet
+    ; mutable gen_exprs: Instr.TempSet
+    ; mutable kill_exprs: Instr.TempSet
+    ; mutable use_vars: Instr.TempSet
+    ; mutable def_vars: Instr.TempSet
+    ; mutable live_in: Instr.TempSet
+    ; mutable live_out: Instr.TempSet
     }
 
   let compare bb1 bb2 = compare bb1.id bb2.id
   let hash bb = Hashtbl.hash bb.id
-  let equal bb1 bb2 = bb1.id = bb2.id
+  let equal bb1 bb2 = (=) bb1.id bb2.id
+
+  let compute_gen_defs blk =
+          (* ??? *)
+
+  let compute_kill_defs blk =
+          (* ??? *)
+
+  let compute_gen_exprs blk =
+          (* ??? *)
+
+  let compute_kill_exprs blk = 
+          (* ??? *)
+
+  let compute_use_vars blk =
+          (* ??? *)
+
+  let compute_def_vars blk =
+          (* ??? *)
+
+  let compute_live_in blk dfg =
+          (* ??? *)
+
+  let compute_live_out blk dfg =
+          (* ??? *)
 end
 
 module ControlTransfer = struct
   type t =
-    | ConditionalJump
+    | IfTrueJump
+    | IfFalseJump
+    | Goto
     | Fallthrough
 
   let compare = compare
@@ -23,8 +59,12 @@ module DataTransfer = struct
     | Use of Instr.TempSet.t
     | Def of Instr.TempSet.t
 
-  let compare = Instr.TempSet.compare
-  let default = Instr.TempSet.empty
+  let compare (Use _) (Def _) = false
+  let compare (Def _) (Use _) = false
+  let compare (Use tset1) (Use tset2) = Instr.TempSet.compare tset1 tset2
+  let compare (Def tset1) (Def tset2) = Instr.TempSet.compare tset1 tset2
+
+  let default = Use (Instr.TempSet.empty)
 end
 
 module CFG = Graph.Imperative.Digraph.ConcreteLabeled(BasicBlock)(ControlTransfer)
