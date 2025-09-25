879dc98ec463d7da024aea97f0e9bc8bdba7fcbb
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-09-25 21:38:00 +0330

    Refactors, Revisions, Addictions: the NFA construtor module needs a lot of testing still, adn a lot more development. @REMINDER: factor out NFAConstructor.t.tree

--- lib/ere.ml
+++ lib/ere.ml
@@ -237,7 +237,12 @@ module ERE = struct
       incr uid_counter;
       uid
 
-    let create accepting =
+    let empty =
+      { uid = (-1)
+      ; accepting = false
+      }
+
+    let create ?accepting:(false) () =
       { uid = next_uid ()
       ; accepting = accepting
       }
@@ -260,7 +265,8 @@ module ERE = struct
 
   module NFAConstructor = struct
     type t =
-      { nfa: NFA.t
+      { start_state: AutomatonState.t
+      ; graph: NFA.t
       ; tree: Absyn.t
       }
 
@@ -270,18 +276,24 @@ module ERE = struct
       }
 
     let create tree =
-      { nfa = NFA.create ~size:20
-      ; tree = tree
+      { start_state: AutomatonState.empty
+      ; graph = NFA.create ~size:20
+      ; tree = tree (* TODO: Remove `tree` *)
       }
 
     let add_transition nfacon src lbl dst =
-      NFA.add_vertex nfacon.nfa src;
-      NFA.add_vertex nfacon.nfa dst;
-      NFA.add_edge_e nfacon.nfa (src, lbl, dst)
+      NFA.add_vertex nfacon.graph src;
+      NFA.add_vertex nfacon.graph dst;
+      NFA.add_edge_e nfacon.graph (src, lbl, dst)
 
-    let rec build_nfa nfacon =
+    let build_nfa nfacon =
+      let fragment = build_fragment nfacon nfacon.tree in
+      fragment.accept.accepting <- true;
+      { nfacon with start_state = fragment.start }
+
+    and build_fragment nfacon node =
       let frag =
-        match nfacon.tree with
+        match node with
         | Absyn.Literal c -> fragment_literal nfacon c
         | Absyn.Wildcard -> fragment_wildcard nfacon
         | Absyn.StartAnchor -> fragment_start_anchor nfacon
@@ -295,111 +307,111 @@ module ERE = struct
       frag
 
     and fragment_literal nfacon literal_char =
-      let start = AutomatonState.create false in
-      let accept = AutomatonState.create false in
-      add_transition nfacon.nfa start (AutomatonTransition.OnSymbol literal_char) accept;
-      { start; accept }
+      let start = AutomatonState.create in
+      let accept = AutomatonState.create in
+      add_transition nfacon start (AutomatonTransition.OnSymbol literal_char) accept;
+      { start ; accept }
 
     and fragment_wildcard nfacon =
-      let start = AutomatonState.create false in
-      let accept = AutomatonState.create false in
+      let start = AutomatonState.create in
+      let accept = AutomatonState.create in
       add_transition nfacon start AutomatonTransition.OnAny accept;
-      { start; accept }
+      { start ; accept }
 
     and fragment_start_anchor nfacon =
-      let start = AutomatonState.create false in
-      let accept = AutomatonState.create false in
-      add_transition nfacon.nfa start AutomatonState.VirtualStart accept;
-      { start; accept }
+      let start = AutomatonState.create in
+      let accept = AutomatonState.create in
+      add_transition nfacon start AutomatonState.VirtualStart accept;
+      { start ; accept }
 
     and fragment_end_anchor nfacon =
-      let start = AutomatonState.create false in
-      let accept = AutomatonState.create false in
-      add_transition nfacon.nfa start AutomatonState.VirtualEnd accept;
-      { start; accept }
+      let start = AutomatonState.create in
+      let accept = AutomatonState.create in
+      add_transition nfacon start AutomatonState.VirtualEnd accept;
+      { start ; accept }
 
     and fragment_repeat nfacon repeat_subexpr min_repeat max_repeat =
       let min = match min_repeat with Some x -> x | None -> 0 in
       let max = match max_repeat with Some x -> x | None -> (-1) in
-      let start = AutomatonState.create false in
-      let accept = AutomatonState.create false in
+      let start = AutomatonState.create in
+      let accept = AutomatonState.create in
       if max = 0 then (
-        add_transition nfacon.nfa start AutomatonTransition.Epsilon accept;
-        { start; accept }
+        add_transition nfacon start AutomatonTransition.Epsilon accept;
+        { start ; accept }
       ) else if min = 0 && (max = 1) then (
         let frag = build_nfa { nfacon with tree = repeat_subexpr } in
-        add_transition nfacon.nfa start AutomatonTransition.Epsilon frag.start;
-        add_transition nfacon.nfa frag.accept AutomatonTransition.Epsilon accpet;
-        add_transition nfacon.nfa start AutomatonTransition.Epsilon accept;
-        { start; accept }
+        add_transition nfacon start AutomatonTransition.Epsilon frag.start;
+        add_transition nfacon frag.accept AutomatonTransition.Epsilon accpet;
+        add_transition nfacon start AutomatonTransition.Epsilon accept;
+        { start ; accept }
       ) else if min = 0 && max = (-1) then (
         let frag = build_nfa { nfacon with tree = repeat_subexpr } in
-        add_transition nfacon.nfa start AutomatonTransition.Epsilon frag.start;
-        add_transition nfacon.nfa frag.accept AutomatonTransition.Epsilon frag.start;
-        add_transition nfacon.nfa frag.accept AutomatonTransition.Epsilon accpet;
-        { start; accept }
+        add_transition nfacon start AutomatonTransition.Epsilon frag.start;
+        add_transition nfacon frag.accept AutomatonTransition.Epsilon frag.start;
+        add_transition nfacon frag.accept AutomatonTransition.Epsilon accpet;
+        { start ; accept }
       ) else if max > 0 then (
         let rec chain i prev =
           if i = 0 then prev
           else
             let frag = build_nfa { nfacon with tree = repeat_subexpr } in
-            add_transition nfacon.nfa prev AutomatonTransition.Epsilon frag.start;
+            add_transition nfacon prev AutomatonTransition.Epsilon frag.start;
             chain (i - 1) frag.accept
         in
         let after_min = chain min start in
         if max = min then (
-          add_transition nfacon.nfa after_min AutomatonTransition.Epsilon accept;
-          { start; accept }
+          add_transition nfacon after_min AutomatonTransition.Epsilon accept;
+          { start ; accept }
         ) else (
           let rec optional k prev =
             if k = 0 then prev
             else
               let frag = build_nfa { nfacon with tree = repeat_subexpr } in
-              add_transition nfacon.nfa prev AutomatonTransition.Epsilon frag.start;
-              add_transition nfacon.nfa prev AutomatonTransition.Epsilon accpet;
+              add_transition nfacon prev AutomatonTransition.Epsilon frag.start;
+              add_transition nfacon prev AutomatonTransition.Epsilon accpet;
               optional (k - 1) frag.accept
           in
           let last = optional (max - min) after_min in
-          add_transition nfacon.nfa last AutomatonTransition.Epsilon accept;
-          { start; accept }
+          add_transition nfacon last AutomatonTransition.Epsilon accept;
+          { start ; accept }
         ) 
       ) else (
         let frag = build_nfa { nfacon with tree = repeat_subexpr } in
-        add_transition nfacon.nfa start AutomatonTransition.Epsilon frag.start;
-        add_transition nfacon.nfa frag.accept AutomatonTransition.Epsilon frag.start;
-        add_transition nfacon.nfa frag.accept AutomatonTransition.Epsilon accpet;
-        add_transition nfacon.nfa start AutomatonTransition.Epsilon acccept;
-        { start; accept }
+        add_transition nfacon start AutomatonTransition.Epsilon frag.start;
+        add_transition nfacon frag.accept AutomatonTransition.Epsilon frag.start;
+        add_transition nfacon frag.accept AutomatonTransition.Epsilon accpet;
+        add_transition nfacon start AutomatonTransition.Epsilon acccept;
+        { start ; accept }
       )
 
     and fragment_brack nfacon brack_chars negated =
-      let start = AutomatonState.create false in
-      let accept = AutomatonState.create false in
+      let start = AutomatonState.create in
+      let accept = AutomatonState.create in
       if negated
-      then add_transition nfacon.nfa start AutomatonTransition.OnAny accept
+      then add_transition nfacon start AutomatonTransition.OnAny accept
       else 
         List.iter (fun c ->
-            add_transition nfacon.nfa start (AutomatonTransition.OnSymbol c) accept) brack_chars;
-      { start; accept }
+            add_transition nfacon start (AutomatonTransition.OnSymbol c) accept) brack_chars;
+      { start ; accept }
 
     and fragment_group nfacon group_subexpr =
       fragment_concat nfacon group_subexpr
 
     and fragment_alt nfacon left_node right_node =
-      let start = AutomatonState.create false in
-      let accept = AutomatonState.create false in
+      let start = AutomatonState.create in
+      let accept = AutomatonState.create in
       let left_frag = build_nfa { nfacon with tree = left_node } in
       let right_frag = build_nfa { nfacon with tree = right_node } in
-      add_transition nfacon.nfa start AutomatonTransition.Epsilon left_frag;
-      add_transition nfacon.nfa start AutomatonTransition.Epsilon right_frag;
-      add_transition nfacon.nfa left_frag.accept AutomatonTransition.Epsilon accept;
-      add_transition nfacon.nfa right_frag.accept AutomatonTransition.Epsilon accept;
-      { start; accept }
+      add_transition nfacon start AutomatonTransition.Epsilon left_frag;
+      add_transition nfacon start AutomatonTransition.Epsilon right_frag;
+      add_transition nfacon left_frag.accept AutomatonTransition.Epsilon accept;
+      add_transition nfacon right_frag.accept AutomatonTransition.Epsilon accept;
+      { start ; accept }
 
     and fragment_concat nfacon cat_exprs =
       match cat_exprs with
       | [] ->
-        let start = AutomatonState.create false in
+        let start = AutomatonState.create in
         { start ; accept = start }
       | head :: tail ->
         let first_frag = build_nfa { nfacon with tree = head } in
@@ -407,7 +419,7 @@ module ERE = struct
           | [] -> prev_accept
           | x :: xs ->
             let frag = build_nfa { nfacon with tree = x } in
-            add_transition nfacon.nfa prev_accept AutomatonTransition.Epsilon frag.start
+            add_transition nfacon prev_accept AutomatonTransition.Epsilon frag.start
               connect frags frag.accept xs
         in
         let accept = connect nfacon first_frag.accept tail in
