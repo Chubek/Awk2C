4c1d91d1208a19e1824d8a0edfd612b17328237a
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-09-26 10:10:40 +0330

    Added, Revised, Refactored: the old functions were all rewritten with some help from Claude Opus

--- lib/ere.ml
+++ lib/ere.ml
@@ -125,8 +125,8 @@ module ERE = struct
       | '(' ->
         let sub_expr = parse_ere ere in
         if not (consume_if ere ')')
-        then raise Syntax_error;
-        Absyn.Group sub_expr
+        then raise Syntax_error
+        else Absyn.Group sub_expr
       | '[' -> parse_bracket ere
       | '*' | '+' | '?' | '{' | '|' | ')' -> raise Unescaped_operator
       | _ as ord_char -> Absyn.Literal ord_char
@@ -267,8 +267,8 @@ module ERE = struct
 
   module NFAConstructor = struct
     type t =
-      { mutable start_state: AutomatonState.t
-      ; mutable accept_state: AutomatonState.t
+      { start_state: AutomatonState.t
+      ; accept_state: AutomatonState.t
       ; graph: NFA.t
       }
 
@@ -276,6 +276,12 @@ module ERE = struct
       { start_state = AutomatonState.empty
       ; accept_state = AutomatonState.empty
       ; graph = NFA.create ()
+      }
+
+    let create_fragment start_state accept_state graph =
+      { start_state = start_state
+      ; accept_state = accept_state
+      ; graph = graph
       }
 
     let add_transition nfacon src lbl dst =
@@ -295,5 +301,179 @@ module ERE = struct
       | Absyn.StartAnchor -> build_start_anchor nfacon
       | Absyn.EndAnchor -> build_end_anchor nfacon
 
+    and build_exact nfacon expr n =
+      let open Absyn in
+      if n <= 0 then
+        let new_start = AutomatonState.create () in
+        let new_accept = AutomatonState.create () ~accepting:true in
+        add_transition nfacon new_start Epsilon new_accept;
+        { start_state = new_start
+        ; accept_state = new_accept
+        ; graph = nfacon.graph
+        }
+      else 
+        let rec build_chain count current_nfa =
+          if count = n then current_nfa
+          else
+            let next_nfa = build_nfa nfacon expr in
+            add_transition nfacon current_nfa.accept_state Epsilon next_nfa.start_state;
+            build_chain (count + 1) (create_fragment new_start new_accept nfacib.graph)
+        in
+        let first_nfa = build_nfa nfacon expr in
+        build_chain 1 first_nfa
+
+    and build_concat nfacon lst =
+      let open Absyn in
+      match lst with
+      | [] ->
+        let new_start = AutomatonState.create () in
+        let new_accept = AutomatonState.create ~accpeting:true () in
+        add_transition nfacon new_start Epsilon new_accept;
+      | [single] -> build_nfa nfacon single
+      | head :: tail ->
+        let nfa_first = build_nfa nfacon head in
+        let rec concat_rest nfas rest =
+          match rest with
+          | [] -> nfas
+          | x :: xs ->
+            let nfa_next = build_nfa nfacon x in
+            add_transition nfacon nfas.accept_state Epsilon nfa_next.start_state;
+            concat_rest (create_fragment nfas.start_state nfas.accept_state nfacon.graph) xs in
+        concat_rest nfa_first tl
+
+    and build_group nfacon expr =
+      build_nfa nfacon expr
+
+    and uild_brack nfacon lst neg =
+      let open Absyn in
+      let new_start = AutomatonState.create () in
+      let new_accept = AutomatonState.create ~accepting:true () in
+
+      if neg then
+        for i = 0 to 127 do
+          let ch = Char.chr i in
+          if not (List.mem ch lst) then
+            add_transition nfacon new_start (OnSymbol ch) new_accept
+        done
+      else
+        List.iter (fun ch -> add_transition new_start (OnSymbol ch) new_accept) lst;
+      { start_state = new_start
+      ; accept_state = new_accept
+      ; graph = nfacon.graph
+      }
+
+    and build_wildcard nfacon =
+      let open Absyn in
+      let new_start = AutomatonState.create () in
+      let new_accept = AutomatonState.create ~accepting:true () in
+      for i = 32 to 126 do
+        let ch = Char.chr i in
+        if ch != '\n' then
+          add_transition nfacon new_start (OnSymbol ch) new_accept
+      done;
+      (* add_transition nfacon new_start (OnSymbol '\t') new_accept *)
+      { start_state = new_start
+      ; accept_state = new_accept
+      ; graph = nfacon.graph
+      }
+
+    and build_start_anchor nfacon =
+      let open Absyn in
+      let new_start = AutomatonState.create () in
+      let new_accept = AutomatonState.create ~accepting:true () in
+      add_transition nfacon new_start VirtualStart new_accept;
+      { start_state = new_start; accept_state = new_accept; graph = nfacon.graph }
+
+    and build_end_anchor nfacon =
+      let open Absyn in
+      let new_start = AutomatonState.create () in
+      let new_accept = AutomatonState.create ~accepting:true () in
+      add_transition nfacon new_start VirtualEnd new_accept;
+      { start_state = new_start; accept_state = new_accept; graph = nfacon.graph }
+
+    and build_repeat nfacon expr min max =
+      let open Absyn in
+      match min, max with
+      | 0, -1 ->
+        let sub_nfa = build_nfa nfacon expr in
+        let new_start = AutomatonState.create () in
+        let new_accept = AutomatonState.create ~accepting:true () in
+        add_transition nfacon new_start Epsilon new_accept;
+        add_transition nfacon new_start Epsilon sub_nfa.start_state;
+        add_transition nfacon sub_nfa.accept_state Epsilon sub_nfa.start_state;
+        add_transition nfacon sub_nfa.accept_state Epsilon new_accept;
+        { start_state = new_start; accept_state = new_accept; graph = nfacon.graph }
+
+      | 1, -1 ->
+        let sub_nfa = build_nfa nfacon expr in
+        let new_accept = AutomatonState.create ~accepting:true () in
+        add_transition nfacon sub_nfa.accept_state Epsilon sub_nfa.start_state;
+        add_transition nfacon sub_nfa.accept_state Epsilon new_accept;
+        { start_state = sub_nfa.start_state; accept_state = new_accept; graph = nfacon.graph }
+
+      | 0, 1 ->
+        let sub_nfa = build_nfa nfacon expr in
+        let new_start = AutomatonState.create () in
+        let new_accept = AutomatonState.create ~accepting:true () in
+        add_transition nfacon new_start Epsilon new_accept;
+        add_transition nfacon new_start Epsilon sub_nfa.start_state;
+        add_transition nfacon sub_nfa.accept_state Epsilon new_accept;
+        { start_state = new_start; accept_state = new_accept; graph = nfacon.graph }
+
+      | n, m when n = m && n >= 0 ->
+        build_exact nfacon expr n
+
+      | n, -1 when n > 0 ->
+        let nfa_n_times = build_exact nfacon expr n in
+        let nfa_star = build_repeat nfacon expr 0 (-1) in
+        add_transition nfacon nfa_n_times.accept_state Epsilon nfa_star.start_state;
+        { start_state = nfa_n_times.start_state
+        ; accept_state = nfa_star.accept_state
+        ; graph = nfacon.graph 
+        }
+
+      | n, m when n >= 0 && m > n ->
+        let nfa_mandatory = build_exact nfacon expr n in
+
+        let rec build_optional count current_state =
+          if count = 0 then
+            current_state
+          else
+            let opt_nfa = build_nfa nfacon expr in
+            let new_state = AutomatonState.create ~accepting:true () in
+            add_transition nfacon current_state Epsilon opt_nfa.start_state;
+            add_transition nfacon opt_nfa.accept_state Epsilon new_state;
+            add_transition nfacon current_state Epsilon new_state;
+            build_optional (count - 1) new_state
+        in
+
+        let final_accept = 
+          if n = 0 then
+            build_optional m nfa_mandatory.start_state
+          else
+            build_optional (m - n) nfa_mandatory.accept_state
+        in
+
+        { start_state = nfa_mandatory.start_state
+        ; accept_state = final_accept
+        ; graph = nfacon.graph 
+        }
+
+      | 0, 0 ->
+        let new_start = AutomatonState.create () in
+        let new_accept = AutomatonState.create ~accepting:true () in
+        add_transition nfacon new_start Epsilon new_accept;
+        { start_state = new_start; accept_state = new_accept; graph = nfacon.graph }
+
+      | _, _ ->
+        build_nfa nfacon expr
+
+    let from_ast ast =
+      let open Absyn in
+      let nfacon = create () in
+      let nfa = build_nfa nfacon ast in
+      add_transition nfacon nfacon.start_state Epsilon nfa.start_state;
+      add_transition nfacon nfa.accept_state Epsilon nfacon.accept_state;
+      nfacon
   end
 end
