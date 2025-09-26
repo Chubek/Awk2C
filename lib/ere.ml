ad26da8259255adec885187a25132554ebf402e7
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-09-26 04:44:35 +0330

    Revised, Removed: the entire code for `build_nfa` was removed for revision. Changed `t * int option * int option` for `Absyn.Repeat` into `t * int * int`, now `-1` means "NONE"

--- lib/ere.ml
+++ lib/ere.ml
@@ -4,7 +4,7 @@ module ERE = struct
       | Group of t list
       | Literal of char
       | Brack of char list * bool
-      | Repeat of t * int option * int option
+      | Repeat of t * int * int
       | Concat of t list
       | Alt of t * t
       | Wildcard
@@ -96,12 +96,12 @@ module ERE = struct
         match (peek_opt ere) with
         | Some '*' ->
           consume ere |> ignore;
-          (Some 0, None)
+          (0, -1)
         | Some '+' ->
           consume ere |> ignore;
-          (Some 1, None)
+          (1, -1)
         | Some '?' ->
-          (Some 0, Some 1)
+          (0, 1)
         | Some '{' ->
           consume ere |> ignore;
           let n = parse_number ere in
@@ -109,9 +109,11 @@ module ERE = struct
           let m = parse_number ere in
           consume_if ere '}';
           (n, m)
-        | _ -> (None, None)
+        | _ -> (-1, -1)
       in
-      Absyn.Repeat (atom_node, min, max)
+      if (min = -1) && (max = -1)
+      then atom_node
+      else Absyn.Repeat (atom_node, min. max)
 
     and parse_atom ere =
       let ch = consume ere in
@@ -132,16 +134,16 @@ module ERE = struct
     and parse_number ere =
       let num_chars = consume_while ere (fun ch -> ch >= '0' && ch <= '9') in
       if List.is_empty num_chars
-      then None
-      else Some (Awk2CUtils.string_to_integer 10 (String.of_seq (List.to_seq num_chars)))
+      then -1
+      else (Awk2CUtils.string_to_integer 10 (String.of_seq (List.to_seq num_chars)))
 
     and parse_hex ere =
       let hex_chars = consume_while ere (fun ch -> (ch >= '0' && ch <= '9') 
                                                    && (ch >= 'a' && ch <= 'f')
                                                    && (ch >= 'A' && ch <= 'F')) in
       if List.is_empty hex_chars 
-      then None
-      else Some (Awk2CUtils.string_to_integer 16 (String.of_seq (List.to_seq hex_chars)))
+      then 0
+      else (Awk2CUtils.string_to_integer 16 (String.of_seq (List.to_seq hex_chars)))
 
     and parse_bracket ere =
       let negated = consume_if ere '^' in
@@ -265,165 +267,21 @@ module ERE = struct
 
   module NFAConstructor = struct
     type t =
-      { start_state: AutomatonState.t
+      { mutable start_state: AutomatonState.t
+      ; mutable accept_state: AutomatonState.t
       ; graph: NFA.t
-      ; tree: Absyn.t
       }
 
-    and fragment =
-      { start: NFA.vertex
-      ; accept: NFA.vertex
-      }
-
-    let create tree =
-      { start_state: AutomatonState.empty
-      ; graph = NFA.create ~size:20
-      ; tree = tree (* TODO: Remove `tree` *)
+    let create () =
+      { start_state = AutomatonState.empty
+      ; accept_state = AutomatonState.empty
+      ; graph = NFA.create ()
       }
 
     let add_transition nfacon src lbl dst =
       NFA.add_vertex nfacon.graph src;
       NFA.add_vertex nfacon.graph dst;
       NFA.add_edge_e nfacon.graph (src, lbl, dst)
-
-    let build_nfa nfacon =
-      let fragment = build_fragment nfacon nfacon.tree in
-      fragment.accept.accepting <- true;
-      { nfacon with start_state = fragment.start }
-
-    and build_fragment nfacon node =
-      let frag =
-        match node with
-        | Absyn.Literal c -> fragment_literal nfacon c
-        | Absyn.Wildcard -> fragment_wildcard nfacon
-        | Absyn.StartAnchor -> fragment_start_anchor nfacon
-        | Absyn.EndAnchor -> fragment_end_anchor nfacon
-        | Absyn.Brack (lst, neg) -> fragment_brack nfacon lst neg
-        | Absyn.Repeat (sub, min, max) -> fragment_repeat nfacon sub min max
-        | Absyn.Group lst -> fragment_group nfacon lst
-        | Absyn.Alt (l, r) -> fragment_alt nfacon l r
-        | Absyn.Concat lst -> fragment_concat nfacon lst
-      in
-      frag
-
-    and fragment_literal nfacon literal_char =
-      let start = AutomatonState.create in
-      let accept = AutomatonState.create in
-      add_transition nfacon start (AutomatonTransition.OnSymbol literal_char) accept;
-      { start ; accept }
-
-    and fragment_wildcard nfacon =
-      let start = AutomatonState.create in
-      let accept = AutomatonState.create in
-      add_transition nfacon start AutomatonTransition.OnAny accept;
-      { start ; accept }
-
-    and fragment_start_anchor nfacon =
-      let start = AutomatonState.create in
-      let accept = AutomatonState.create in
-      add_transition nfacon start AutomatonState.VirtualStart accept;
-      { start ; accept }
-
-    and fragment_end_anchor nfacon =
-      let start = AutomatonState.create in
-      let accept = AutomatonState.create in
-      add_transition nfacon start AutomatonState.VirtualEnd accept;
-      { start ; accept }
-
-    and fragment_repeat nfacon repeat_subexpr min_repeat max_repeat =
-      let min = match min_repeat with Some x -> x | None -> 0 in
-      let max = match max_repeat with Some x -> x | None -> (-1) in
-      let start = AutomatonState.create in
-      let accept = AutomatonState.create in
-      if max = 0 then (
-        add_transition nfacon start AutomatonTransition.Epsilon accept;
-        { start ; accept }
-      ) else if min = 0 && (max = 1) then (
-        let frag = build_nfa { nfacon with tree = repeat_subexpr } in
-        add_transition nfacon start AutomatonTransition.Epsilon frag.start;
-        add_transition nfacon frag.accept AutomatonTransition.Epsilon accpet;
-        add_transition nfacon start AutomatonTransition.Epsilon accept;
-        { start ; accept }
-      ) else if min = 0 && max = (-1) then (
-        let frag = build_nfa { nfacon with tree = repeat_subexpr } in
-        add_transition nfacon start AutomatonTransition.Epsilon frag.start;
-        add_transition nfacon frag.accept AutomatonTransition.Epsilon frag.start;
-        add_transition nfacon frag.accept AutomatonTransition.Epsilon accpet;
-        { start ; accept }
-      ) else if max > 0 then (
-        let rec chain i prev =
-          if i = 0 then prev
-          else
-            let frag = build_nfa { nfacon with tree = repeat_subexpr } in
-            add_transition nfacon prev AutomatonTransition.Epsilon frag.start;
-            chain (i - 1) frag.accept
-        in
-        let after_min = chain min start in
-        if max = min then (
-          add_transition nfacon after_min AutomatonTransition.Epsilon accept;
-          { start ; accept }
-        ) else (
-          let rec optional k prev =
-            if k = 0 then prev
-            else
-              let frag = build_nfa { nfacon with tree = repeat_subexpr } in
-              add_transition nfacon prev AutomatonTransition.Epsilon frag.start;
-              add_transition nfacon prev AutomatonTransition.Epsilon accpet;
-              optional (k - 1) frag.accept
-          in
-          let last = optional (max - min) after_min in
-          add_transition nfacon last AutomatonTransition.Epsilon accept;
-          { start ; accept }
-        ) 
-      ) else (
-        let frag = build_nfa { nfacon with tree = repeat_subexpr } in
-        add_transition nfacon start AutomatonTransition.Epsilon frag.start;
-        add_transition nfacon frag.accept AutomatonTransition.Epsilon frag.start;
-        add_transition nfacon frag.accept AutomatonTransition.Epsilon accpet;
-        add_transition nfacon start AutomatonTransition.Epsilon acccept;
-        { start ; accept }
-      )
-
-    and fragment_brack nfacon brack_chars negated =
-      let start = AutomatonState.create in
-      let accept = AutomatonState.create in
-      if negated
-      then add_transition nfacon start AutomatonTransition.OnAny accept
-      else 
-        List.iter (fun c ->
-            add_transition nfacon start (AutomatonTransition.OnSymbol c) accept) brack_chars;
-      { start ; accept }
-
-    and fragment_group nfacon group_subexpr =
-      fragment_concat nfacon group_subexpr
-
-    and fragment_alt nfacon left_node right_node =
-      let start = AutomatonState.create in
-      let accept = AutomatonState.create in
-      let left_frag = build_nfa { nfacon with tree = left_node } in
-      let right_frag = build_nfa { nfacon with tree = right_node } in
-      add_transition nfacon start AutomatonTransition.Epsilon left_frag.start;
-      add_transition nfacon start AutomatonTransition.Epsilon right_frag.start;
-      add_transition nfacon left_frag.accept AutomatonTransition.Epsilon accept;
-      add_transition nfacon right_frag.accept AutomatonTransition.Epsilon accept;
-      { start ; accept }
-
-    and fragment_concat nfacon cat_exprs =
-      match cat_exprs with
-      | [] ->
-        let start = AutomatonState.create in
-        { start ; accept = start }
-      | head :: tail ->
-        let first_frag = build_nfa { nfacon with tree = head } in
-        let rec connect frags prev_accept = function
-          | [] -> prev_accept
-          | x :: xs ->
-            let frag = build_nfa { nfacon with tree = x } in
-            add_transition nfacon prev_accept AutomatonTransition.Epsilon frag.start
-              connect frags frag.accept xs
-        in
-        let accept = connect nfacon first_frag.accept tail in
-        { start = first_frag.start ; accept }
 
   end
 end
